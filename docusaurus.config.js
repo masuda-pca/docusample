570f9148747c99f87c55f5547905f52e265c3a10
y-masuda <y-masuda@pca.co.jp>
2021-10-05 17:48:11 +0900

    [Add] documentation.ymml [Modify] docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,14 +6,14 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 (module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://masuda-pca.github.io',
+  baseUrl: '/docusample/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'masuda-pca', // Usually your GitHub org/user name.
+  projectName: 'docusample', // Usually your repo name. 
+  trailingSlash: false,
   presets: [
     [
       '@docusaurus/preset-classic',
